89497b40ac83bfd5ab490523d9f0ad093472a039
Max <max@supergene.co>
2026-09-15 09:55:16 +0900

    weight 열 미검증 항목을 정본 v1.5 확정값으로 닫고 T10 주석에 원작 대응임을 명시

--- data.js
+++ data.js
@@ -466,7 +466,7 @@ const SECTIONS = [
         project: "smg",
         status: "초안",
         version: "정본 1.5",
-        updated: "2026-09-15 09:41",
+        updated: "2026-09-15 11:20",
         pinned: false,
         tags: ["머지", "인터랙티브", "밸런스"],
       },
